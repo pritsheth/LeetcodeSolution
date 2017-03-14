a1dfe2113778466534d85a182b9cb8d6cb9371b7
pritsheth <pritsheth99@gmail.com>
2017-03-14 22:00:17 +0530

    Relative sorting

---
+++ Sorting/src/RelativeSorting.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created by Jerry on 27-02-2017.
+ */
+public class RelativeSorting {
+    public String[] findRelativeRanks(int[] nums) {
+
+      //  Arrays.sort(nums,new Comparator<>());
+return null;
+
+    }
+}
